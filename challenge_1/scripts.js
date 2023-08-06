539e6595dd1f53b39ad8660fc3c13b960ac92e5f
khomotso gojela <motsoak49@gmail.com>
2023-08-06 15:24:29 +0200

    challenge_1

---
+++ challenge_1/scripts.js
@@ -0,0 +1,24 @@
+// const primaryPhone = 'O748105141'
+// const secondaryPhone = '0219131568'
+
+// // Only change below this line
+
+// const primaryValid = typeof number == primaryPhone 
+// const secondaryValid = typeof number == secondaryPhone
+
+// console.log('Primary phone is valid numerical string:', primaryValid)
+// console.log('Secondary phone is valid numerical string:', secondaryValid )
+/*******************************************************************************************************/
+
+// solution
+
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = !isNaN(primaryPhone)
+const secondaryValid = !isNaN(secondaryPhone)
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
